c75a132b251115bff33acd26b278380cfd3b4675
Na Lin <lna483018@gmail.com>
2023-02-26 21:32:05 -0500

    exercies: create and use TodoItem component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+// Component
+import TodoItem from "./TodoItem";
+
+// Data Model
 import Todo from "../models/todo";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
